dafd7efbc138214a4194c94da25cc523af9ef1fd
cbm27 <baomei.cui27@qmail.cuny.edu>
2025-05-06 21:14:12 -0400

    second commit

---
+++ 343CODE/customer/src/app/components/CustomersList.jsx
@@ -0,0 +1,12 @@
+export default function CustomersList() {
+    return (
+      <>
+       Add New Customer
+      <p>Jane Doe</p> 
+      <p>John Smith</p>
+      <p>Lisa Baker</p>
+
+     
+      </>
+    );
+  }
---
+++ 343CODE/customer/src/app/page.jsx
@@ -0,0 +1,10 @@
+import CustomersList from "./components/CustomersList";
+
+export default function Home() {
+  return (
+    <>
+    <h1>All customers</h1> 
+    CustomersList
+    </>
+  );
+}
