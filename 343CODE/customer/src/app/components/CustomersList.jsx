37ee3d4db7399befc1fd5f34b602758e7e675fcd
cbm27 <baomei.cui27@qmail.cuny.edu>
2025-05-12 12:39:37 -0400

    link to add customers

--- 343CODE/customer/src/app/components/CustomersList.jsx
+++ 343CODE/customer/src/app/components/CustomersList.jsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 export default function CustomersList() {
     return (
       <>
+       <Link
        Add New Customer
       <p>Jane Doe</p> 
       <p>John Smith</p>
