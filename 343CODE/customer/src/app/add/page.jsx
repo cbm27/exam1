9e032b14a20e0a46b72d297cd6851fc2f9cea4ad
cbm27 <baomei.cui27@qmail.cuny.edu>
2025-05-12 14:48:01 -0400

    added form tag

--- 343CODE/customer/src/app/add/page.jsx
+++ 343CODE/customer/src/app/add/page.jsx
@@ -2,11 +2,13 @@ export default function Page() {
   return (
     <>
       <h1>Add Customer</h1>
+    <form>
     <label htmlFor="fname">First Name</label>
     <input id="fname" type= "text" name="first _ name"/>
     <label htmlFor="lname">List Name</label>
     <input id="lname" type= "text" name="last _ name"/>
     <button>Save</button>
+    </form>
     </>
   );
 }
